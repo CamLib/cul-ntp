b7c58b2fc997a345de805e5b7097f117828b2eef
Peter Heiner <p.heiner@gmail.com>
2015-02-23 16:10:57 +0000

    try anchors

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,8 +6,10 @@ class ntp (
     $configfile = '/etc/ntp.conf',
     $servers    = [],
 ) {
+    anchor { 'ntp::begin': } ->
     class{'ntp::install': } ->
     class{'ntp::config': } ~>
     class{'ntp::service': } ->
-    Class["ntp"]
+    anchor { 'ntp::end': }
+#    Class["ntp"]
 }
